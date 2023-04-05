ff7562eb438cae45064f9c38fb3e9a0c5680d550
Andy <ajscottlewis@gmail.com>
2023-04-05 12:06:51 +0100

    Refactor: iterate through words array to check all words

--- spellChecker.js
+++ spellChecker.js
@@ -5,17 +5,18 @@ class SpellChecker {
   }
 
   checkSpelling() {
-    if (this._input === 'One twwo') {
-      return 'One ~twwo~'
-    } else if (this._input === 'Test two') {
-      return 'Test two';
-    }
+    const words = this._input.split(' ');
+    let outputArray = [];
 
-    if (this._dictionary.includes(this._input.toLowerCase())) {
-      return this._input;
-    } else {
-      return `~${this._input}~`;
-    }
+    words.forEach((word) => {
+      if (this._dictionary.includes(word.toLowerCase())) {
+        outputArray.push(word);
+      } else {
+        outputArray.push(`~${word}~`);
+      }
+    })
+
+    return outputArray.join(' ');
   }
 }
 
