67e01efa982196ceb40ac19fed591cd7208a9edb
Andy <ajscottlewis@gmail.com>
2023-04-05 11:49:07 +0100

    Returns correctly spelt word as is

--- spellChecker.js
+++ spellChecker.js
@@ -1,5 +1,11 @@
 class SpellChecker {
+  constructor(input) {
+    this._input = input;
+  }
 
+  checkSpelling() {
+    return this._input;
+  }
 }
 
 module.exports = SpellChecker;
--- spellChecker.test.js
+++ spellChecker.test.js
@@ -1,5 +1,9 @@
 const SpellChecker = require('./spellChecker');
 
 describe('SpellChecker', () => {
-
+  it('outputs a correctly spelt word', () => {
+    const spellChecker = new SpellChecker('test');
+    
+    expect(spellChecker.checkSpelling()).toEqual('test');
+  })
 })
